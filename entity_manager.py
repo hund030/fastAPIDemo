ae70a5865d2fcb6950596baf9be7b7eabbf5817f
hd00300 <hd00300@sjtu.edu.cn>
2021-02-11 01:02:18 +0800

    feat: query entities

--- entity_manager.py
+++ entity_manager.py
@@ -1,32 +1,47 @@
 from azure.data.tables import TableClient
+import random
+import copy
 
-entity = {
-    u'PartitionKey': u'color',
-    u'RowKey': u'brand',
-    u'text': u'Marker',
-    u'color': u'Purple',
-    u'price': u'5'
+brands = [u"Crayola", u"Sharpie", u"Chameleon"]
+colors = [u"red", u"blue", u"orange", u"yellow"]
+names = [u"marker", u"pencil", u"pen"]
+entity_template = {
+    u"PartitionKey": u"pk",
+    u"RowKey": u"row",
 }
 
 
 class EntityManger(object):
     @staticmethod
-    def create_entity(client: TableClient) -> bool:
+    def create_entity(client: TableClient, row_key: str) -> bool:
+        e = copy.deepcopy(entity_template)
+        e[u"RowKey"] = row_key
+        e[u"Name"] = random.choice(names)
+        e[u"Brand"] = random.choice(brands)
+        e[u"Color"] = random.choice(colors)
+        e[u"Value"] = random.randint(0, 100)
+
         from azure.core.exceptions import ResourceExistsError
         try:
-            client.create_entity(entity=entity)
+            client.create_entity(entity=e)
             return True
         except ResourceExistsError:
             print("Entity already exists.")
             return False
 
     @staticmethod
-    def delete_entity(client: TableClient) -> bool:
+    def query_entities(client: TableClient) -> list:
+        queried_entities = client.query_entities(filter="")
+        for entity_chosen in queried_entities:
+            print(entity_chosen)
+        return queried_entities
+
+    @staticmethod
+    def delete_entity(client: TableClient, row_key: str, partition_key: str) -> bool:
         from azure.core.exceptions import ResourceNotFoundError
         try:
-            client.delete_entity(row_key=entity["RowKey"],
-                                 partition_key=entity["PartitionKey"])
+            client.delete_entity(row_key=row_key, partition_key=partition_key)
             return True
         except ResourceNotFoundError:
-            print("Entity does not exists.")
+            print("Entity does not exist.")
             return False
--- main.py
+++ main.py
@@ -22,24 +22,30 @@ def create_table(table_name: str):
         raise HTTPException(status_code=400, detail='Table already exists.')
 
 
-@app.post("/entities")
-def create_entity(table_name: str):
-    client = ClientFactory.getTableClient(table_name)
-    result = EntityManger.create_entity(client)
-    if not result:
-        raise HTTPException(status_code=400, detail='Entity already exists.')
-
-
-@app.delete("/entities")
-def delete_entity(table_name: str):
-    client = ClientFactory.getTableClient(table_name)
-    result = EntityManger.delete_entity(client)
-    raise HTTPException(status_code=400, detail='Entity does not exist.')
-
-
 @app.delete("/tables")
 def delete_table(table_name: str):
     client = ClientFactory.getTableServiceClient()
     result = TableManager.delete_table(client, table_name)
     if not result:
         raise HTTPException(status_code=400, detail='Table does not exist.')
+
+
+@app.get("/entities")
+def query_entities(table_name: str):
+    client = ClientFactory.getTableClient(table_name)
+    result = EntityManger.query_entities(client)
+
+
+@app.post("/entities")
+def create_entity(table_name: str, row_key: str):
+    client = ClientFactory.getTableClient(table_name)
+    result = EntityManger.create_entity(client, row_key)
+    if not result:
+        raise HTTPException(status_code=400, detail='Entity already exists.')
+
+
+@app.delete("/entities")
+def delete_entity(table_name: str, row_key: str, partition_key: str):
+    client = ClientFactory.getTableClient(table_name)
+    result = EntityManger.delete_entity(client, row_key, partition_key)
+    raise HTTPException(status_code=400, detail='Entity does not exist.')
