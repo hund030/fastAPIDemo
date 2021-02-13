c655d86bccc63ac28f057280b73c35ab53b6823f
Huiming Qiu <47179776+qiuhuiming@users.noreply.github.com>
2021-02-13 11:51:42 +0800

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-account_name = 'mylibrary030'
-account_key = '5A+DFIq31sIXE0CU9rZwT04p/xn9OXb7Cw36IKfI77o7YkX33SWGnFQHf3DUXcWeV0RbcDH3NiqdXhv4urDTBg=='
-connection_string = 'DefaultEndpointsProtocol=[http|https];AccountName=%s;AccountKey=%s' % (account_name, account_key)
-class Config:
-    @staticmethod
-    def get_connection_string():
-        return connection_string
