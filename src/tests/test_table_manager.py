c7872a65fa0c96cba9d10b1aaff5c01b94baee45
hd00300 <hd00300@sjtu.edu.cn>
2021-03-30 13:55:22 +0800

    test: add test for table manager

--- src/tests/test_table_manager.py
+++ src/tests/test_table_manager.py
@@ -1,9 +1,31 @@
-from src.ops import ClientFactory
+from src.ops import ClientFactory, TableManager
+from src.resources import Config
+
+
+class MockTable:
+    @staticmethod
+    @property
+    def table_name():
+        return 'name'
+
 
 class TestTableManager:
-    def test_create_table(self):
-        client = ClientFactory.getTableServiceClient()
-        assert True
+    def create_table(*args, **kwargs):
+        return MockTable
 
-    def test_delete_table(self):
-        assert True 
+    def get_connection_string(*args, **kwargs):
+        return ''
+
+    def test_create_table(self, monkeypatch):
+        client = ClientFactory.getTableServiceClient()
+        monkeypatch.setattr(client, 'create_table', self.create_table)
+        monkeypatch.setattr(Config, 'get_connection_string', self.get_connection_string)
+
+        assert TableManager.create_table(client, 'name')
+
+    def test_delete_table(self, monkeypatch):
+        client = ClientFactory.getTableServiceClient()
+        monkeypatch.setattr(client, 'delete_table', self.create_table)
+        monkeypatch.setattr(Config, 'get_connection_string', self.get_connection_string)
+        
+        assert TableManager.delete_table(client, 'name')
